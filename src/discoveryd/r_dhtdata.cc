3ffb4b92ea304fa02fef8cfa8a3939c1e9f3ed5b
Lucas Dias <lucas_dias@redes.ufsm.br>
2024-06-13 18:01:15 -0300

    Change encrypt method for encrypt then auth
    encryption use rand values to avoid attacks
    like known-plaintext. Thus, sign encrypted data
    will generate different signature for same
    plaintext data.
    For this case, encrypt then auth is more appropriate.
    
    Signed-off-by: Lucas Dias <lucas_dias@redes.ufsm.br>

--- src/discoveryd/r_dhtdata.cc
+++ src/discoveryd/r_dhtdata.cc
@@ -10,14 +10,14 @@ namespace riaps {
         void DhtData::EncryptData(std::vector<uint8_t> &data,
                                   std::shared_ptr<dht::crypto::PrivateKey> private_key) {
             auto public_key = private_key->getPublicKey();
-            signature = private_key->sign(data);
             encrypted_data = public_key.encrypt(data);
+            signature = private_key->sign(encrypted_data);
         }
 
         bool DhtData::DecryptData(std::shared_ptr<dht::crypto::PrivateKey> private_key) {
             try {
-                auto data = private_key->decrypt(encrypted_data);
-                if (private_key->getPublicKey().checkSignature(data, signature)) {
+                if (private_key->getPublicKey().checkSignature(encrypted_data, signature)) {
+                    auto data = private_key->decrypt(encrypted_data);
                     raw_data = data;
                     return true;
                 }
